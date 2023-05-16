deb595e7582306716c1c14e81d8f1acf5a560737
NUWANI SUDARSHIKA <nuwanisudarshi@gmail.com>
2023-05-16 16:07:11 +0530

    Interface practical code

---
+++ src/InterfaceOne.java
@@ -0,0 +1,24 @@
+interface InterfaceOne {
+    public abstract void methodOne();
+}
+interface InterfaceTwo{
+    public abstract void methodTwo();
+}
+class Inheritance implements InterfaceOne,InterfaceTwo{
+    @Override
+    public void methodOne() {
+        System.out.println("1");
+    }
+
+    @Override
+    public void methodTwo() {
+        System.out.println("2");
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        Inheritance i= new Inheritance();
+        i.methodOne();
+        i.methodTwo();
+    }
+}
